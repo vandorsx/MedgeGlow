d0bf402f8250832b58f7d6345e25492d28880849
Jade van Dorsten <jade@inthetrees.me>
2024-08-13 21:48:11 -0500

    Clean up AppDelegate

--- medgeglow/AppDelegate.swift
+++ medgeglow/AppDelegate.swift
@@ -4,121 +4,34 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
    var statusItem: NSStatusItem?
    var overlayWindow: NSWindow?
-   let settings = OverlaySettings()
    var preferencesWindowController: PreferencesWindowController?
    var popover: NSPopover?
    
+   let settings = OverlaySettings()
+   
    func applicationDidFinishLaunching(_ notification: Notification) {
+      setupUIComponents()
+   }
+   
+   // MARK: - Setup Methods
+   
+   private func setupUIComponents() {
       setupStatusItem()
       setupOverlayWindow()
       setupPreferencesWindow()
       setupPopover()
-      
-      NSApplication.shared.mainMenu = buildMenu()
    }
    
-   func setupPreferencesWindow() {
-      preferencesWindowController = PreferencesWindowController(settings: settings)
-   }
-   
-   @objc func showPreferences() {
-      preferencesWindowController?.showWindow(nil)
-   }
-   
-   @objc func showAboutPanel() {
-      NSApplication.shared.orderFrontStandardAboutPanel(nil)
-   }
-   
-   
-   func setupStatusItem() {
+   private func setupStatusItem() {
       statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-      if let button = statusItem?.button {
-         button.image = NSImage(systemSymbolName: "rectangle.inset.filled", accessibilityDescription: nil)
-         button.action = #selector(handleStatusItemAction(_:))
-         button.sendAction(on: [.leftMouseUp, .rightMouseUp])
-      }
-   }
-   
-   @objc func handleStatusItemAction(_ sender: NSStatusBarButton) {
-      if let event = NSApp.currentEvent {
-         if event.type == .rightMouseUp {
-            let menu = createRightClickMenu()
-            statusItem?.menu = menu
-            statusItem?.button?.performClick(nil)
-            statusItem?.menu = nil
-         } else {
-            toggleOverlay()
-         }
-      }
-   }
-   
-   @objc func toggleMenu() {
-      if statusItem?.menu == nil {
-         statusItem?.menu = createMenu()
-      } else {
-         statusItem?.menu = nil
-      }
+      guard let button = statusItem?.button else { return }
       
-      statusItem?.button?.performClick(nil)
+      button.image = NSImage(systemSymbolName: "rectangle.inset.filled", accessibilityDescription: nil)
+      button.action = #selector(handleStatusItemAction(_:))
+      button.sendAction(on: [.leftMouseUp, .rightMouseUp])
    }
    
-   func setupPopover() {
-      popover = NSPopover()
-      popover?.contentSize = NSSize(width: 300, height: 200)
-      popover?.behavior = .transient
-      popover?.contentViewController = NSHostingController(rootView: ContentView().environmentObject(settings))
-   }
-   
-   @objc func togglePopover(_ sender: NSStatusBarButton) {
-      if let event = NSApp.currentEvent {
-         if event.type == .rightMouseUp {
-            let menu = createRightClickMenu()
-            statusItem?.menu = menu
-            statusItem?.button?.performClick(nil)
-            statusItem?.menu = nil
-         } else {
-            if let popover = popover {
-               if popover.isShown {
-                  popover.performClose(sender)
-               } else {
-                  popover.show(relativeTo: sender.bounds, of: sender, preferredEdge: .minY)
-               }
-            }
-         }
-      }
-   }
-   
-   func createRightClickMenu() -> NSMenu {
-      let menu = NSMenu()
-      menu.addItem(NSMenuItem(title: "Preferences...", action: #selector(showPreferences), keyEquivalent: ","))
-      menu.addItem(NSMenuItem.separator())
-      menu.addItem(NSMenuItem(title: "About MedgeGlow", action: #selector(showAboutPanel), keyEquivalent: ""))
-      menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-      return menu
-   }
-   
-   
-   func createMenu() -> NSMenu {
-      let menu = NSMenu()
-      
-      menu.addItem(NSMenuItem(title: "Toggle Overlay", action: #selector(toggleOverlay), keyEquivalent: ""))
-      menu.addItem(NSMenuItem(title: "Preferences...", action: #selector(showPreferences), keyEquivalent: ","))
-      menu.addItem(NSMenuItem.separator())
-      menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-      
-      return menu
-   }
-   
-   @objc func toggleOverlay() {
-      if overlayWindow?.isVisible == true {
-         overlayWindow?.orderOut(nil)
-      } else {
-         overlayWindow?.makeKeyAndOrderFront(nil)
-      }
-   }
-   
-   
-   func setupOverlayWindow() {
+   private func setupOverlayWindow() {
       let screen = NSScreen.main ?? NSScreen.screens[0]
       let frame = NSRect(x: 0, y: 0, width: screen.frame.width, height: screen.frame.height)
       
@@ -128,34 +41,85 @@ class AppDelegate: NSObject, NSApplicationDelegate {
       overlayWindow?.backgroundColor = .clear
       overlayWindow?.ignoresMouseEvents = true
       
-      let hostingView = NSHostingView(rootView:
-                                       OverlayView()
-         .environmentObject(settings)
-      )
+      let hostingView = NSHostingView(rootView: OverlayView().environmentObject(settings))
       overlayWindow?.contentView = hostingView
       overlayWindow?.makeKeyAndOrderFront(nil)
    }
    
+   private func setupPreferencesWindow() {
+      preferencesWindowController = PreferencesWindowController(settings: settings)
+   }
+   
+   private func setupPopover() {
+      popover = NSPopover()
+      popover?.contentSize = NSSize(width: 300, height: 200)
+      popover?.behavior = .transient
+      popover?.contentViewController = NSHostingController(rootView: ContentView().environmentObject(settings))
+   }
+   
+   // MARK: - Menu Actions
+   
+   @objc private func handleStatusItemAction(_ sender: NSStatusBarButton) {
+      guard let event = NSApp.currentEvent else { return }
+      
+      if event.type == .rightMouseUp {
+         showRightClickMenu()
+      } else {
+         toggleOverlay()
+      }
+   }
+   
+   private func showRightClickMenu() {
+      let menu = createRightClickMenu()
+      statusItem?.menu = menu
+      statusItem?.button?.performClick(nil)
+      statusItem?.menu = nil
+   }
+   
+   @objc private func togglePopover(_ sender: NSStatusBarButton) {
+      guard let event = NSApp.currentEvent else { return }
+      
+      if event.type == .rightMouseUp {
+         showRightClickMenu()
+      } else if let popover = popover {
+         if popover.isShown {
+            popover.performClose(sender)
+         } else {
+            popover.show(relativeTo: sender.bounds, of: sender, preferredEdge: .minY)
+         }
+      }
+   }
+   
+   @objc private func toggleOverlay() {
+      if overlayWindow?.isVisible == true {
+         overlayWindow?.orderOut(nil)
+      } else {
+         overlayWindow?.makeKeyAndOrderFront(nil)
+      }
+   }
+   
+   @objc private func showPreferences() {
+      preferencesWindowController?.showWindow(nil)
+   }
+   
+   @objc private func showAboutPanel() {
+      NSApplication.shared.orderFrontStandardAboutPanel(nil)
+   }
+   
+   // MARK: - Menu Creation
+   
+   private func createRightClickMenu() -> NSMenu {
+      let menu = NSMenu()
+      menu.addItem(NSMenuItem(title: "Preferences...", action: #selector(showPreferences), keyEquivalent: ","))
+      menu.addItem(NSMenuItem.separator())
+      menu.addItem(NSMenuItem(title: "About MedgeGlow", action: #selector(showAboutPanel), keyEquivalent: ""))
+      menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+      return menu
+   }
+   
+   // MARK: - NSApplicationDelegate
+   
    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
       return false
-   }
-}
-
-extension AppDelegate {
-   func buildMenu() -> NSMenu {
-      let mainMenu = NSMenu()
-      
-      let appMenuItem = NSMenuItem()
-      appMenuItem.submenu = NSMenu(title: "App")
-      mainMenu.addItem(appMenuItem)
-      
-      let appMenu = appMenuItem.submenu!
-      appMenu.addItem(NSMenuItem(title: "About MedgeGlow", action: #selector(NSApplication.orderFrontStandardAboutPanel(_:)), keyEquivalent: ""))
-      appMenu.addItem(NSMenuItem.separator())
-      appMenu.addItem(NSMenuItem(title: "Settings...", action: #selector(showPreferences), keyEquivalent: ","))
-      appMenu.addItem(NSMenuItem.separator())
-      appMenu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-      
-      return mainMenu
    }
 }
